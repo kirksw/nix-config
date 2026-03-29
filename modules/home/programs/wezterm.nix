490251866ac8840316c898392e415939ba3c334d
Kirk Sweeney <kirk@cntd.io>
2026-03-29 22:48:04 +0200

    fix wezterm appearance sync

--- modules/home/programs/wezterm.nix
+++ modules/home/programs/wezterm.nix
@@ -5,6 +5,9 @@
   ...
 }:
 
+let
+  appearanceModeFile = "${config.xdg.stateHome}/appearance/mode";
+in
 {
   options = {
     homeModules.wezterm.enable = lib.mkEnableOption "enables wezterm";
@@ -19,8 +22,36 @@
       extraConfig = ''
         local wezterm = require("wezterm")
         local action = wezterm.action
+        local config = {}
+        local appearance_mode_file = ${builtins.toJSON appearanceModeFile}
 
-        return {
+        wezterm.add_to_config_reload_watch_list(appearance_mode_file)
+
+        local function read_mode()
+          local file = io.open(appearance_mode_file, "r")
+          if not file then
+            return "dark"
+          end
+
+          local mode = file:read("*l")
+          file:close()
+
+          if mode == "light" then
+            return "light"
+          end
+
+          return "dark"
+        end
+
+        local function scheme_for_mode(mode)
+          if mode == "dark" then
+            return "Rosé Pine (Gogh)"
+          end
+
+          return "Rosé Pine Dawn (Gogh)"
+        end
+
+        config = {
           animation_fps = 120,
           max_fps = 120,
           front_end = "WebGpu",
@@ -28,7 +59,7 @@
           window_decorations = "RESIZE",
           font = wezterm.font("FiraCode Nerd Font Mono"),
           font_size = 14.0,
-          color_scheme = "Rosé Pine (Gogh)",
+          color_scheme = scheme_for_mode(read_mode()),
           window_background_opacity = 0.85,
           macos_window_background_blur = 20,
           window_close_confirmation = 'NeverPrompt',
@@ -70,6 +101,8 @@
             },
           },
         }
+
+        return config
       '';
     };
   };
